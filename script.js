927b50eed166c196f37d09f1342002e1ecf6c81c
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-08-10 23:22:50 +0530

    add final style, add final working button with functionality. final submission.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const sketchPad = document.querySelector('#sketch-pad')
 const resetButton = document.querySelector('.reset')
-const gridbox = '';
-let gridBoxColor = '';
-
+const classicButton = document.querySelector('.classic')
+const randomButton = document.querySelector('.random')
 let input = parseInt(prompt("Enter the number of grids you want !"))
 
 function inputChecker() {
@@ -13,15 +12,17 @@ function inputChecker() {
 }
 
 function randomColorGenerator () {
+    //generates 3 random numbers for a random RGB color
     let num1 = Math.floor(Math.random()*1000)
     let num2 = Math.floor(Math.random()*1000)
     let num3 = Math.floor(Math.random()*1000)
     
-    gridBoxColor = `rgb(${num1},${num2},${num3})`
+    let gridBoxColor = `rgb(${num1},${num2},${num3})`
     return gridBoxColor;
 }
 
-function gridGenerator() {
+function randomColorGridGenerator() {
+    
     for (i=0;i<(input*input);i++) { 
         //loop that adds number of divs as per user specified number
         const gridbox = document.createElement('div');
@@ -40,6 +41,27 @@ function gridGenerator() {
     }
 }
 
+function classicColorGridGenerator() {
+    
+    for (i=0;i<(input*input);i++) { 
+        //loop that adds number of divs as per user specified number
+        const gridbox = document.createElement('div');
+        gridbox.classList.add('gridbox');
+        sketchPad.appendChild(gridbox);
+    
+        //calculates the side size that fits the sketchpad.
+        let calculatedside = Math.sqrt(250000/(input*input))
+        gridbox.style.width = calculatedside+"px";
+        gridbox.style.height = calculatedside+"px";
+         
+        //Listens for mouse hovering
+        gridbox.addEventListener("mouseover", function pen () {
+        gridbox.style.backgroundColor = 'black';
+        })
+    }
+}
+
+
 function gridRemover() {
     for (i=0;i<(input*input);i++) {
         sketchPad.removeChild(sketchPad.firstChild);
@@ -49,16 +71,28 @@ function gridRemover() {
 
 
 inputChecker()
-gridGenerator()
+classicColorGridGenerator()
 
 resetButton.addEventListener('click', function clear () {
     
     gridRemover()
     input = parseInt(prompt("Enter the number of grids you want !"))
     inputChecker()
-    gridGenerator()
+    classicColorGridGenerator()
     }
 )
 
+classicButton.addEventListener('click', function () {
+    
+    gridRemover()
+    classicColorGridGenerator()
+    
+})
 
+randomButton.addEventListener('click', function () {
+    
+    gridRemover()
+    randomColorGridGenerator()
+    
+})
 
